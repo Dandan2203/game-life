cb0ad461257a72b0cb039d42db1a137867f10bf9
zipz221_mpd <petromaizen@gmail.com>
2024-04-19 03:17:49 +0100

    game-life v1.0

---
+++ game-life/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace game_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SpeedReg = new System.Windows.Forms.TrackBar();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedReg)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SpeedReg);
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Controls.Add(this.StopButton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Швидкість";
+            // 
+            // SpeedReg
+            // 
+            this.SpeedReg.Location = new System.Drawing.Point(12, 32);
+            this.SpeedReg.Maximum = 500;
+            this.SpeedReg.Minimum = 25;
+            this.SpeedReg.Name = "SpeedReg";
+            this.SpeedReg.Size = new System.Drawing.Size(135, 45);
+            this.SpeedReg.TabIndex = 2;
+            this.SpeedReg.Value = 50;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.Location = new System.Drawing.Point(3, 508);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(144, 41);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StopButton.Location = new System.Drawing.Point(3, 555);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(144, 41);
+            this.StopButton.TabIndex = 0;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(156, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 600);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 602);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedReg)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar SpeedReg;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ game-life/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace game_life
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private bool[,] field;
+        private const int rows  = 40;
+        private const int cols  = 40;
+        private const int cellSize = 15;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGane()
+        {
+            if (timer.Enabled)
+                return;
+
+            field = new bool[cols, rows];
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer.Start();
+        }
+
+        private void Generation()
+        {
+            graphics.Clear(Color.White);
+
+            var newField = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = Neighbours(x, y);
+                    var lifeNeighbour = field[x, y];
+
+                    if(!lifeNeighbour && neighboursCount == 3)
+                        newField[x, y] = true;
+                    else if ( lifeNeighbour && (neighboursCount < 2 || neighboursCount > 3) )   
+                        newField[x, y] = false;
+                    else
+                        newField[x, y] = field[x, y];
+
+                    if (lifeNeighbour)
+                        graphics.FillRectangle(Brushes.Black, x * cellSize, y * cellSize, cellSize - 1, cellSize - 1);
+                }
+            }
+            field = newField;
+            pictureBox1.Refresh();
+        }
+
+        private int Neighbours(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1;i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + j + rows) % rows;
+
+                    var isSelfCheck = col == x && row == y;
+                    var lifeNeighbour = field[col, row];
+
+                    if (lifeNeighbour && !isSelfCheck)
+                        count++;
+                }
+            }
+            return count;
+        }
+        private void StopGame()
+        {
+            if (!timer.Enabled)
+                return;
+            timer.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Generation();
+            timer.Interval = SpeedReg.Value;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartGane();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer.Enabled) 
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / 15;
+                var y = e.Location.Y / 15;
+                var validationPassed = ValidateMousePos(x, y);
+                if (validationPassed)
+                    field[x, y] = true;
+            }
+            
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / 15;
+                var y = e.Location.Y / 15;
+                var validationPassed = ValidateMousePos(x, y);
+                if (validationPassed)
+                    field[x, y] = false;
+            }
+        }
+
+        private bool ValidateMousePos(int x , int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+    }
+}
